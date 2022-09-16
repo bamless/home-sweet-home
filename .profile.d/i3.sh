6954a15c901f58a22737d3c1647bc971a52cbfcb
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2022-09-17 01:13:33 +0200

    Customizations for the `think-fedora` machine

---
+++ .profile.d/i3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "${XDG_CURRENT_DESKTOP}" == "i3" ]; then
+    export QT_AUTO_SCREEN_SCALE_FACTOR=0
+    export QT_SCREEN_SCALE_FACTORS=2 
+    export GDK_SCALE=2
+    export GDK_DPI_SCALE=0.5
+    # Set theme (GTK4 Workaround)
+    export GTK_THEME=WhiteSur-Dark-solid-blue-nord #Flat-Remix-GTK-Blue-Dark-Solid
+fi
