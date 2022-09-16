b78cc67b3bd23c5c6a491418eb31af4b4038dcbc
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2022-09-17 01:13:33 +0200

    Customizations for the `think-fedora` machine

--- .config/nvim/lua/config/plugins/dap.lua
+++ .config/nvim/lua/config/plugins/dap.lua
@@ -144,7 +144,7 @@ local function setup_dap()
         {
             type = "chrome",
             request = "launch",
-            url = "http://localhost:3000",
+            url = "http://localhost:5173",
             program = "${file}",
             cwd = vim.fn.getcwd(),
             sourceMaps = true,
@@ -158,7 +158,7 @@ local function setup_dap()
         {
             type = "chrome",
             request = "launch",
-            url = "http://localhost:3000",
+            url = "http://localhost:5173",
             program = "${file}",
             cwd = vim.fn.getcwd(),
             sourceMaps = true,
