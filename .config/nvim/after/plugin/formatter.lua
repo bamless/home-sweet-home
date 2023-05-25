8a56b3d9e32ed955e21fa0163d8921990bd09fd3
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-05-25 15:19:09 +0200

    Better formatter configuration for neovim

--- .config/nvim/after/plugin/formatter.lua
+++ .config/nvim/after/plugin/formatter.lua
@@ -1,6 +1,21 @@
 -- Utilities for creating configurations
 --local util = require "formatter.util"
 
+local formatters = {
+    -- Use the special "*" filetype for defining formatter configurations on
+    -- any filetype
+    ["*"] = {
+        -- "formatter.filetypes.any" defines default configurations for any
+        -- filetype
+        require("formatter.filetypes.any").remove_trailing_whitespace
+    }
+}
+
+local configs = require('config.formatters')
+for lang, config in pairs(configs) do
+    formatters[lang] = config
+end
+
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
 require("formatter").setup {
     -- Enable or disable logging
@@ -8,17 +23,5 @@ require("formatter").setup {
     -- Set the log level
     log_level = vim.log.levels.WARN,
     -- All formatter configurations are opt-in
-    filetype = {
-        python = {
-            require('formatter.filetypes.python').autopep8,
-        },
-
-        -- Use the special "*" filetype for defining formatter configurations on
-        -- any filetype
-        ["*"] = {
-            -- "formatter.filetypes.any" defines default configurations for any
-            -- filetype
-            require("formatter.filetypes.any").remove_trailing_whitespace
-        }
-    }
+    filetype = formatters,
 }
