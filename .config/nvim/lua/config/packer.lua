92fab31bd8eb2cbc9c19c929b5714357137c51ac
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-05-26 14:49:41 +0200

    Add `nvim-dap-virtual-text` plugin to neovim

--- .config/nvim/lua/config/packer.lua
+++ .config/nvim/lua/config/packer.lua
@@ -98,6 +98,7 @@ return require('packer').startup(function(use)
     ---
 
     use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
+    use 'theHamsta/nvim-dap-virtual-text'
 
     ---
     -- Color Themes
