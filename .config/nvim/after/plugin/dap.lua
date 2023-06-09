c3863ab25b67130060a67267d4db19ede508d612
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-06-09 12:14:55 +0200

    Change some neovim settings

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -3,8 +3,6 @@ local dapui = require('dapui')
 
 dapui.setup()
 dap.listeners.after.event_initialized["dapui_config"]=function() vim.cmd("NvimTreeToggle"); dapui.open() end
-dap.listeners.before.event_terminated["dapui_config"]=function() vim.cmd("NvimTreeToggle"); dapui.close() end
-dap.listeners.before.event_exited["dapui_config"]=function() vim.cmd("NvimTreeToggle"); dapui.close() end
 
 require("nvim-dap-virtual-text").setup()
 
