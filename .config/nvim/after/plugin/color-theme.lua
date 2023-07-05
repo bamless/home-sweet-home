4c9368bf652b664a529a02a280232fd1270500d8
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-07-05 16:05:51 +0200

    Ligten up dim color in `tokyonight` neovim theme

--- .config/nvim/after/plugin/color-theme.lua
+++ .config/nvim/after/plugin/color-theme.lua
@@ -1,4 +1,12 @@
-require('tokyonight').setup { style = "moon" }
+require('tokyonight').setup {
+    style = "moon",
+    on_colors = function(colors)
+       -- colors.terminal_black = "#717ba8"
+       -- dim color is too dim, lighten it up
+       colors.terminal_black = colors.dark5
+       colors.dark3 = colors.dark5
+    end
+}
 require('catppuccin').setup {}
 
 function ColorTheme(color)
