f3b396295b04fa96987cff8e2f9994fa65d716eb
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-07-17 01:17:57 +0200

    Switch to `clangd` as C/C++ language server

--- .config/nvim/lua/config/lsp/ccls.lua
+++
@@ -1,8 +0,0 @@
-return function()
-    require('lspconfig').ccls.setup {
-        on_attach = require('config.lsp').on_attach,
-        init_options = {
-            compilationDatabaseDirectory = "build",
-        },
-    }
-end
