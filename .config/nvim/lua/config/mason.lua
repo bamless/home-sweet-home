ae50a05179ac36a78da2f9011fc3b5e298d16877
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-09-29 00:27:07 +0200

    Add list of Mason packages to install in neovim config

---
+++ .config/nvim/lua/config/mason.lua
@@ -0,0 +1,18 @@
+local options = {
+    ensure_installed = {
+        'autopep8',
+        'chrome-debug-adapter',
+        'clangd',
+        'cpptools',
+        'eslint-lsp',
+        'gopls',
+        'jdtls',
+        'json-lsp',
+        'lua-language-server',
+        'pyright',
+        'rust-analyzer',
+        'typescript-language-server',
+    },
+}
+
+return options;
