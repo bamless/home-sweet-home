c884b275e465a872323f930c43e4cca4b3550dd3
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-09-28 16:45:37 +0200

    Minor changes to tokyonight theme colors

--- .config/nvim/after/plugin/color-theme.lua
+++ .config/nvim/after/plugin/color-theme.lua
@@ -1,10 +1,11 @@
 require('tokyonight').setup {
     style = "night",
     on_colors = function(colors)
-       -- colors.terminal_black = "#717ba8"
-       -- dim color is too dim, lighten it up
-       colors.terminal_black = colors.dark5
-       colors.dark3 = colors.dark5
+        -- colors.terminal_black = "#717ba8"
+        -- dim color is too dim, lighten it up
+        colors.terminal_black = colors.dark5
+        colors.dark3 = colors.dark5
+        colors.terminal_black = colors.bg_dark
     end
 }
 require('catppuccin').setup {}
