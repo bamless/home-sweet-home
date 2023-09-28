31d0f20698b2a79001f9a38578ddca095c2e956d
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-09-28 12:53:33 +0200

    Changed neovim configrations

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -66,23 +66,86 @@ require('luasnip').config.set_config({
 require('luasnip.loaders.from_vscode').lazy_load()
 
 ---
--- Autocompletion
+-- Autocompletion (cmp)
 ---
-
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 local cmp = require('cmp')
 
-local cmp_config = lsp_zero.defaults.cmp_config({})
--- Limit size of autocompletion popup
+local function border(hl_name)
+    return {
+        { "╭", hl_name },
+        { "─", hl_name },
+        { "╮", hl_name },
+        { "│", hl_name },
+        { "╯", hl_name },
+        { "─", hl_name },
+        { "╰", hl_name },
+        { "│", hl_name },
+    }
+end
+
+local cmp_config = lsp_zero.defaults.cmp_config({
+    completion = {
+        completeopt = "menu,menuone",
+    },
+
+    window = {
+        completion = {
+            side_padding = 1,
+            scrollbar = false,
+            border = border "CmpBorder",
+        },
+        documentation = {
+            border = border "CmpDocBorder",
+            winhighlight = "Normal:CmpDoc",
+        },
+    },
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
+
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "nvim_lua" },
+        { name = "path" },
+    },
+})
+
+-- Custom formatting
 cmp_config.formatting = {
-  format = function(_, vim_item)
-    vim_item.abbr = string.sub(vim_item.abbr, 1, 140)
-    return vim_item
-  end
+    -- default fields order i.e completion word + item.kind + item.kind icons
+    fields = { "abbr", "kind", "menu" },
+
+    format = function(entry, item)
+        local icons = require("config.lsp-icons")
+
+        -- Kind icons
+        item.kind = string.format('%s %s', icons[item.kind], item.kind) -- This concatonates the icons with the name of the item kind
+
+        -- Source
+        item.menu = ({
+            buffer = "[Buffer]",
+            nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
+            nvim_lua = "[Lua]",
+            latex_symbols = "[LaTeX]",
+        })[entry.source.name]
+
+        item.abbr = string.sub(item.abbr, 1, 140)
+        return item
+    end
 }
 -- table.insert(cmp_config.sources, { name = 'nvim_lsp_signature_help' })
+
 cmp.setup(cmp_config)
+
+---
+-- Signature hint
+---
 
 require('lsp_signature').setup {
     bind = true, -- This is mandatory, otherwise border config won't get registered.
