9d29538a902071b14bfe48e8d0525fb41de6e958
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-10-25 15:37:01 +0200

    neovim: changed python configuration and enabled auto format

--- .config/nvim/lua/config/lsp/pylsp.lua
+++ .config/nvim/lua/config/lsp/pylsp.lua
@@ -4,18 +4,21 @@ return function()
             pylsp = {
                 plugins = {
                     -- linter options
-                    pylint = { enabled = false },
-                    pyflakes = { enabled = false },
-                    pycodestyle = { enabled = false },
-                    flake8 = { enabled = false },
                     -- Rewrite it in rust!
-                    ruff = { enabled = true },
+                    ruff = {
+                        enabled = true,
+                        extendSelect = { "I" },
+                        lineLength = 100,
+                    },
                     -- type checker
                     pylsp_mypy = {
                         enabled = true,
                         overrides = { '--ignore-missing-imports', '--check-untyped-defs', true },
                         live_mode = true
                     },
+                    -- Formatting
+                    yapf = { enabled = true },
+                    autopep8 = { enabled = false },
                     -- import sorting
                     pyls_isort = { enabled = true },
                     -- This is *really* slow, disable it for now
