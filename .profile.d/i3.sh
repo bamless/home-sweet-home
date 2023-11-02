d20f8fe2412abfbac1f1bcb7674e2beca8ddc58c
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2023-11-02 17:54:48 +0100

    Update i3 conf

---
+++ .profile.d/i3.sh
@@ -0,0 +1,8 @@
+#1/bin/bash
+
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+export QT_SCREEN_SCALE_FACTORS=2 
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
+# Set theme (GTK4 Workaround)
+export GTK_THEME=WhiteSur-Dark-solid-blue-nord #Flat-Remix-GTK-Blue-Dark-Solid
