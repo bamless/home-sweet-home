c7c1d9a1594ec185f5479775482bf8c94a54057a
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2024-01-23 21:07:57 +0100

    Add fzf dependecy to telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,7 @@
+require('telescope').setup {}
+require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-g>', builtin.git_branches, {})
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -38,10 +38,15 @@ return require('lazy').setup({
     { 'L3MON4D3/LuaSnip' },
     { 'neovim/nvim-lspconfig' },
 
+    -- Telescope
     {
         'nvim-telescope/telescope.nvim',
         branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
     },
 
     {
