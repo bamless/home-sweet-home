04eb95bead554fa7fac8cf7af1dae3ea0f952dd1
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2024-04-24 22:08:13 +0200

    Formatting for jdtls

--- .config/nvim/lua/config/lsp/jdtls.lua
+++ .config/nvim/lua/config/lsp/jdtls.lua
@@ -1,5 +1,9 @@
 return function()
     require('lspconfig').jdtls.setup {
+        on_attach = function(client, bufnr)
+            local opts = { buffer = bufnr, remap = false }
+            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+        end,
         settings = {
             java = {
                 signatureHelp = { enabled = true },
