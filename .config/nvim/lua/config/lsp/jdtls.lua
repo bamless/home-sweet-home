52c651e203b59adb3773fc2271d5f471aff46ea9
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2024-06-25 12:30:41 +0200

    nvim: use `nvim-jdtls` instead of lspconfig for setting up java LS

--- .config/nvim/lua/config/lsp/jdtls.lua
+++ .config/nvim/lua/config/lsp/jdtls.lua
@@ -1,19 +1,20 @@
 return function()
-    require('lspconfig').jdtls.setup {
-        on_attach = function(client, bufnr)
-            local opts = { buffer = bufnr, remap = false }
-            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
-        end,
-        settings = {
-            java = {
-                signatureHelp = { enabled = true },
-                completion = {
-                    guessMethodArguments = false
-                },
-                inlayHints = {
-                    parameterNames = { enabled = "literals" }
-                },
-            }
-        }
-    }
+    -- We use nvim-jdtls, so this needs to be empty in order to not setup it two times
+    -- require('lspconfig').jdtls.setup {
+    --     on_attach = function(client, bufnr)
+    --         local opts = { buffer = bufnr, remap = false }
+    --         vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
+    --     end,
+    --     settings = {
+    --         java = {
+    --             signatureHelp = { enabled = true },
+    --             completion = {
+    --                 guessMethodArguments = false
+    --             },
+    --             inlayHints = {
+    --                 parameterNames = { enabled = "literals" }
+    --             },
+    --         }
+    --     }
+    -- }
 end
