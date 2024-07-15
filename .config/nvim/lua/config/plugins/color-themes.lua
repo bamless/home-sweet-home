93a500f800b9f450febc20e7d235c66164278ce5
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2024-07-15 13:22:25 +0200

    nvim: remove custom color configuration from theme

--- .config/nvim/lua/config/plugins/color-themes.lua
+++ .config/nvim/lua/config/plugins/color-themes.lua
@@ -7,12 +7,12 @@ return {
         config = function()
             require('tokyonight').setup {
                 style = "night",
-                on_colors = function(colors)
-                    -- colors.terminal_black = "#717ba8"
-                    -- dim color is too dim, lighten it up
-                    colors.terminal_black = colors.dark5
-                    colors.dark3 = colors.dark5
-                end
+                -- on_colors = function(colors)
+                --     -- colors.terminal_black = "#717ba8"
+                --     -- dim color is too dim, lighten it up
+                --     colors.terminal_black = colors.dark5
+                --     colors.dark3 = colors.dark5
+                -- end
             }
         end
     },
