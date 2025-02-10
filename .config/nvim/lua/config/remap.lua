2249cef60a5d02f7ce74e7eabe734f59b9ecb74d
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-02-10 10:56:51 +0100

    nvim: disable C-j and C-k remap

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -23,8 +23,6 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", "<cmd>Format<CR>")
 vim.keymap.set("n", "<leader>F", "<cmd>Format<CR>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
