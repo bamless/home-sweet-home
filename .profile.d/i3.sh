1fa57c82f74dd5f89472ceb02952e8354787a77e
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-03-25 15:47:11 +0100

    im really getting tired of hidpi scaling

--- .profile.d/i3.sh
+++ .profile.d/i3.sh
@@ -3,8 +3,8 @@
 if [ "${XDG_CURRENT_DESKTOP}" == "i3" ]; then
     export QT_AUTO_SCREEN_SCALE_FACTOR=0
     export QT_SCREEN_SCALE_FACTORS=2 
-    export GDK_SCALE=2
-    export GDK_DPI_SCALE=0.5
+    # export GDK_SCALE=2
+    # export GDK_DPI_SCALE=0.5
     # Set theme (GTK4 Workaround)
     export GTK_THEME=WhiteSur-Dark-solid-blue-nord #Flat-Remix-GTK-Blue-Dark-Solid
 fi
