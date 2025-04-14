a036325d1a567459ed6de9446e1bac1d407093f4
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-04-14 20:13:20 +0200

    nvim: workaround for jdtls dap configuration

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -89,7 +89,9 @@ local config = {
 
     on_attach = function(client, bufnr)
         require("jdtls").setup_dap { hotcodereplace = "auto" }
-        require("jdtls.dap").setup_dap_main_class_configs()
+        -- TODO: this is failing with: Cannot serialise function: type not supported
+        --       check if this is still needed
+        -- require("jdtls.dap").setup_dap_main_class_configs()
 
         require("config.lsp").on_attach(client, bufnr)
 
