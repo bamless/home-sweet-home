603657aae3f71d96f2451641494ff4cf631f1225
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-05-20 21:27:44 +0200

    basedpyright: add type stubs

--- .config/nvim/lua/config/lsp/basedpyright.lua
+++ .config/nvim/lua/config/lsp/basedpyright.lua
@@ -62,6 +62,7 @@ return function()
                     useLibraryCodeForTypes = true,
                     diagnosticMode = 'openFilesOnly',
                     autoImportCompletions = true,
+                    stubPath = vim.fn.stdpath("data") .. "/lazy/typeshed/stubs",
                 },
             },
         },
