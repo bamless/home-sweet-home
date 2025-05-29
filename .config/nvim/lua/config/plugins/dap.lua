f6af5ccccb7baf9fca5d01850c87804cac8ac190
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-05-29 16:10:36 +0200

    dap: enable global breakpoints

--- .config/nvim/lua/config/plugins/dap.lua
+++ .config/nvim/lua/config/plugins/dap.lua
@@ -83,6 +83,8 @@ local function setup_dap()
 
     dap.configurations.python = {
         {
+            -- why is true the default?????
+            justMyCode = false,
             -- The first three options are required by nvim-dap
             type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
             request = 'launch',
