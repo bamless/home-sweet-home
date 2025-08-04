086f37cf3b84d35c6c7303c6297c1e8dfd5a13f1
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-08-04 20:03:27 +0200

    nvim: add more regexes to compile-mode plugin

--- .config/nvim/lua/config/plugins/compile-mode.lua
+++ .config/nvim/lua/config/plugins/compile-mode.lua
@@ -16,8 +16,18 @@ return {
             -- to make `:Compile` replace special characters (e.g. `%`) in
             -- the command (and behave more like `:!`), add:
             bang_expansion = true,
+            -- Custom regexes
+            error_regexp_table = {
+                typescript = {
+                    regex = "^\\(.\\+\\)(\\([1-9][0-9]*\\),\\([1-9][0-9]*\\)): error TS[1-9][0-9]*:",
+                    filename = 1,
+                    row = 2,
+                    col = 3,
+                },
+            }
         }
-        vim.keymap.set("n", "[e", function() vim.cmd[[NextError]] end)
-        vim.keymap.set("n", "]e", function() vim.cmd[[PrevError]] end)
+
+        vim.keymap.set("n", "[e", function() vim.cmd [[NextError]] end)
+        vim.keymap.set("n", "]e", function() vim.cmd [[PrevError]] end)
     end
 }
