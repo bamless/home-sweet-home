610555488b5fa9f229a3eb2d6dead6f708761604
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2025-08-08 19:42:27 +0200

    nvim: minor tweaks to compile-mode plugin

--- .config/nvim/lua/config/plugins/compile-mode.lua
+++ .config/nvim/lua/config/plugins/compile-mode.lua
@@ -12,11 +12,7 @@ return {
     config = function()
         local compile_mode = require "compile-mode"
         vim.g.compile_mode = {
-            environment = {
-                FORCE_COLOR = "1",
-            },
             baleia_setup = true,
-            input_word_completion = true,
             -- to make `:Compile` replace special characters (e.g. `%`) in
             -- the command (and behave more like `:!`), add:
             bang_expansion = true,
