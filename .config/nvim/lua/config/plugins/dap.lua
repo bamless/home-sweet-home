e3f232eb52be146c9d301e086dab828fff361b7e
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2026-01-02 12:32:43 +0100

    nvim: lazy load DAP

--- .config/nvim/lua/config/plugins/dap.lua
+++ .config/nvim/lua/config/plugins/dap.lua
@@ -202,49 +202,75 @@ return {
     {
         "mfussenegger/nvim-dap",
         config = setup_dap,
-        dependencies = { "nvim-neotest/nvim-nio", "rcarriga/nvim-dap-ui" }
-    },
-    {
-        'theHamsta/nvim-dap-virtual-text',
-        opts = {
-            enabled = true,                     -- enable this plugin (the default)
-            enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
-            highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
-            highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-            show_stop_reason = true,            -- show stop reason when stopped for exceptions
-            commented = false,                  -- prefix virtual text with comment string
-            only_first_definition = true,       -- only show virtual text at first definition (if there are multiple)
-            all_references = false,             -- show virtual text on all all references of the variable (not only definitions)
-            clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
-            --- A callback that determines how a variable is displayed or whether it should be omitted
-            display_callback = function(variable, buf, stackframe, node, options)
-                -- by default, strip out new line characters
-                if options.virt_text_pos == 'inline' then
-                    return ' = ' .. variable.value:gsub("%s+", " ")
-                else
-                    return variable.name .. ' = ' .. variable.value:gsub("%s+", " ")
-                end
-            end,
-            -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
-            virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
-
-            -- experimental features:
-            all_frames = false,     -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
-            virt_lines = false,     -- show virtual lines instead of virtual text (will flicker!)
-            virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
-            -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+        keys = {
+            '<F5>',
+            '<leader>br',
+            '<leader>brc',
+            '<leader>lgp',
+            '<leader>dl',
+            '<F10>',
+            '<F11>',
+            '<F12>',
+            '<F6>',
+            '<leader>db',
         },
-        dependencies = { "mfussenegger/nvim-dap" }
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "rcarriga/nvim-dap-ui",
+            {
+                'theHamsta/nvim-dap-virtual-text',
+                opts = {
+                    enabled = true,                     -- enable this plugin (the default)
+                    enabled_commands = true,            -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+                    highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+                    highlight_new_as_changed = false,   -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+                    show_stop_reason = true,            -- show stop reason when stopped for exceptions
+                    commented = false,                  -- prefix virtual text with comment string
+                    only_first_definition = true,       -- only show virtual text at first definition (if there are multiple)
+                    all_references = false,             -- show virtual text on all all references of the variable (not only definitions)
+                    clear_on_continue = false,          -- clear virtual text on "continue" (might cause flickering when stepping)
+                    --- A callback that determines how a variable is displayed or whether it should be omitted
+                    display_callback = function(variable, buf, stackframe, node, options)
+                        -- by default, strip out new line characters
+                        if options.virt_text_pos == 'inline' then
+                            return ' = ' .. variable.value:gsub("%s+", " ")
+                        else
+                            return variable.name .. ' = ' .. variable.value:gsub("%s+", " ")
+                        end
+                    end,
+                    -- position of virtual text, see `:h nvim_buf_set_extmark()`, default tries to inline the virtual text. Use 'eol' to set to end of line
+                    virt_text_pos = vim.fn.has 'nvim-0.10' == 1 and 'inline' or 'eol',
+
+                    -- experimental features:
+                    all_frames = false,     -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+                    virt_lines = false,     -- show virtual lines instead of virtual text (will flicker!)
+                    virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
+                    -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+                },
+                dependencies = { "mfussenegger/nvim-dap" }
+            },
+        }
     },
     {
         "mfussenegger/nvim-dap-python",
-        ft = "python",
         dependencies = {
             "mfussenegger/nvim-dap",
             "rcarriga/nvim-dap-ui",
             "nvim-neotest/nvim-nio",
         },
-        config = function(_, opts)
+        keys = {
+            { '<F5>', ft = "python" },
+            { '<leader>br', ft = "python" },
+            { '<leader>brc', ft = "python" },
+            { '<leader>lgp', ft = "python" },
+            { '<leader>dl', ft = "python" },
+            { '<F10>', ft = "python" },
+            { '<F11>', ft = "python" },
+            { '<F12>', ft = "python" },
+            { '<F6>', ft = "python" },
+            { '<leader>db', ft = "python" },
+        },
+        config = function(_, _)
             local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
             require("dap-python").setup(path)
 
@@ -254,5 +280,5 @@ return {
                 pyconf.justMyCode = false
             end
         end,
-    },
+    }
 }
