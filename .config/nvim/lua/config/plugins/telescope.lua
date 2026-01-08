8828ae758bcd5c2596e4a7c3919aa2bb65cba215
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2026-01-08 01:07:51 +0100

    nvim: tweak telescope config

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -15,6 +15,16 @@ return {
                         '--column',
                         '--smart-case',
                     },
+                    file_ignore_patterns = {
+                        '^node_modules',
+                        '^.git',
+                        '^.venv',
+                        '^dist',
+                        '%.lock$',
+                        '.cache',
+                        '.git',
+                        '.github',
+                    },
                 },
                 pickers = {
                     find_files = {
@@ -22,30 +32,17 @@ return {
                     }
                 }
             }
+
             require('telescope').load_extension('fzf')
             local builtin = require('telescope.builtin')
 
-            -- Find files: first try only git files, then all files if git_files fails
-            vim.keymap.set('n', '<C-p>', function()
-                if not pcall(builtin.git_files) then
-                    builtin.find_files()
-                end
-            end)
-
-            -- Open buffers
             vim.keymap.set('n', '<C-space>', function()
                 builtin.buffers({ sort_mru = true, ignore_current_buffer = true })
             end)
-
+            vim.keymap.set('n', '<C-p>', builtin.find_files)
             vim.keymap.set('n', '<leader>pg', builtin.live_grep)
-
-            -- Git branches
             vim.keymap.set('n', '<C-g>', builtin.git_branches, {})
-            -- Find files in project
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            -- View help tags
             vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-            -- List all marks
             vim.keymap.set('n', '<leader>m', builtin.marks, {})
         end
     },
@@ -54,5 +51,4 @@ return {
         build =
         'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
     },
-
 }
