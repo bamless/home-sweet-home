12496076f4894b90b1107a437be2da3fdf92b8f4
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2026-02-08 13:27:43 +0100

    neovide: minor tweaks

--- .config/nvim/lua/config/opts.lua
+++ .config/nvim/lua/config/opts.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
     vim.g.neovide_position_animation_length = 0
-    vim.g.neovide_cursor_animation_length = 0.00
+    vim.g.neovide_cursor_animation_length = 0.02
     vim.g.neovide_cursor_trail_size = 0
     vim.g.neovide_cursor_animate_in_insert_mode = false
     vim.g.neovide_cursor_animate_command_line = false
