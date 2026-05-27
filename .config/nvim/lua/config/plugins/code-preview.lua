794e1124719cc74d21dde9ec480813e918d3559b
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2026-05-27 12:53:27 +0200

    nvim: minor tweaks to configs

--- .config/nvim/lua/config/plugins/code-preview.lua
+++ .config/nvim/lua/config/plugins/code-preview.lua
@@ -1,6 +1,12 @@
 return {
     "Cannon07/code-preview.nvim",
     config = function()
-        require("code-preview").setup()
+        require("code-preview").setup({
+            keys = {
+                next_change = "[x",         -- inline diff: next change
+                prev_change = "]x",         -- inline diff: previous change
+                close_all   = "<leader>dq", -- close diff and clear indicators
+            },
+        })
     end,
 }
