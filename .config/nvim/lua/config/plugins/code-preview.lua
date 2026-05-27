f40e5422766dcdaea89664381466992d12c4d8dd
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2026-05-27 13:05:22 +0200

    Revert "nvim: minor tweaks to configs"
    This reverts commit 794e1124719cc74d21dde9ec480813e918d3559b.

--- .config/nvim/lua/config/plugins/code-preview.lua
+++ .config/nvim/lua/config/plugins/code-preview.lua
@@ -1,12 +1,6 @@
 return {
     "Cannon07/code-preview.nvim",
     config = function()
-        require("code-preview").setup({
-            keys = {
-                next_change = "[x",         -- inline diff: next change
-                prev_change = "]x",         -- inline diff: previous change
-                close_all   = "<leader>dq", -- close diff and clear indicators
-            },
-        })
+        require("code-preview").setup()
     end,
 }
